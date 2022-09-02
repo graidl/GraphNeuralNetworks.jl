272960da70e1e07251f401ecf4712782c5a25005
Guenther Raidl <raidl@ac.tuwien.ac.at>
2022-09-02 13:11:28 +0200

    MHAConv with one head running

--- examples/node_classification_cora.jl
+++ examples/node_classification_cora.jl
@@ -1,5 +1,7 @@
 # An example of semi-supervised node classification
 
+module Mha
+
 using Flux
 using Flux: onecold, onehotbatch
 using Flux.Losses: logitcrossentropy
@@ -55,8 +57,8 @@ function train(; kws...)
     model1 = GNNChain(GCNConv(nin => nhidden, relu),
                      GCNConv(nhidden => nhidden, relu), 
                      Dense(nhidden, nout))  |> device
-    model2 = GNNChain(TransConv(nin => nhidden, relu),
-                     TransConv(nhidden => nhidden, relu), 
+    model2 = GNNChain(MHAConv(nin => nhidden, relu),
+                     MHAConv(nhidden => nhidden, relu), 
                      Dense(nhidden, nout))  |> device
     model = model2
 
@@ -86,5 +88,35 @@ function train(; kws...)
     end
 end
 
-train()
+function mha()
+    N = 4
+    adj1 =  [0 1 0 1
+             1 0 1 0
+             0 1 0 1
+             1 0 1 0]
+    in_channel = 2
+    out_channel = 1
+    dh = 4
+    heads = 1
+    g = GNNGraph(adj1, ndata=rand(Float32, in_channel, N), graph_type=:sparse)
+    x = node_features(g)
+
+    layer = MHAConv(in_channel => out_channel)
+
+    model = GNNChain(Dense(in_channel => dh),
+        MHAConv(dh => dh; heads),
+        Dense(dh => out_channel))
+
+    y = layer(g, x)
+    @show y
+
+    y = model(g, x)
+    @show y
+end
+
+end  # module
+
+Mha.mha()
+
+# train()
 
